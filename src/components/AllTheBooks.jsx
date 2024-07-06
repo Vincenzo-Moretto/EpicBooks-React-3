aefe1fea7774417b909bc300d4f29ea4395783ef
Vincenzo-Moretto <161637342+Vincenzo-Moretto@users.noreply.github.com>
2024-07-06 13:41:35 +0200

    fix

---
+++ src/components/AllTheBooks.jsx
@@ -0,0 +1,48 @@
+import { FaArrowCircleUp } from "react-icons/fa";
+import fantasy from "../data/fantasy.json";
+import SingleBook from "./SingleBook";
+/* import history from "../data/history.json";
+import horror from "../data/horror.json";
+import romance from "../data/romance.json";
+import scifi from "../data/scifi.json"; */
+
+import { Form, Row } from "react-bootstrap";
+import { useState } from "react";
+
+function AllTheBooks() {
+  const [state, setState] = useState("");
+
+  const filterBookList = (e) => {
+    setState(e.target.value);
+  };
+
+  const arrowup = () => {
+    window.location.href = "#";
+  };
+
+  return (
+    <>
+      <Row>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Carca un libro</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Signore degli anelli, Lo Hobbit..."
+            value={state}
+            onChange={filterBookList}
+          />
+        </Form.Group>
+      </Row>
+      <div className="d-flex justify-content-center flex-wrap gap-5 my-5 pb-5">
+        {[...fantasy /*  ...history, ...horror, ...romance, ...scifi */]
+          .filter((libro) => libro.title.toLowerCase().includes(state.toLocaleLowerCase()))
+          .map((libro) => (
+            <SingleBook key={libro.asin} libro={libro} />
+          ))}
+      </div>
+      <FaArrowCircleUp className="arrowup " onClick={arrowup} />
+    </>
+  );
+}
+
+export default AllTheBooks;
